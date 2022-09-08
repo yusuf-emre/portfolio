7e3f0500a1c859c7eb53b4dd40cfb5ac6d8aa140
Yusuf Emre Velioglu <velioglu.ye@gmail.com>
2022-09-08 08:58:51 +0200

    change directory one layer up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Header from './Component/Head/Header'
 import Home from './Component/Hero/Home'
 import "./App.css"
 
 const App = () => {
+  
+  useEffect(() => {
+    document.title = "Yusuf Emre Velioglu"
+  }, [])
+  
   return (
     <>
       <Header />
