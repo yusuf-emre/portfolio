478ad09a1d98dc52d398ae2593f679596c339a7c
Yusuf Emre Velioglu <velioglu.ye@gmail.com>
2022-12-24 19:11:23 +0100

    add new projects to portfolio

--- src/Component/Portfolio/PortfolioApi.js
+++ src/Component/Portfolio/PortfolioApi.js
@@ -7,9 +7,30 @@ const data = [
 
     },
     {
+        title: "Flight Finder",
+        image: "/flight-finder.gif",
+        description: "Full stack flight finder app to list and book all flights in given specifications",
+        link: "https://github.com/yusuf-emre/flightFinder" 
+
+    },
+    {
+        title: "Employee Information System",
+        image: "/employeeIMS.gif",
+        description: "Employee information management app to reach and manipulate employee data by authentication and user role definition",
+        link: "https://github.com/cetinarslan-it/FullStackEmployeeIMSApp" 
+
+    },
+    {
+        title: "Next.js Product catalog",
+        image: "/product-catalog.gif",
+        description: "Simple shopping app with next.js front end",
+        link: "https://github.com/yusuf-emre/nextjs-product-catalog" 
+
+    },
+    {
         title: "Personal Portfolio",
         image: "/portfolio.gif",
-        description: "My personal portfolio project", 
+        description: "My personal portfolio", 
         link: "https://github.com/yusuf-emre/portfolio"
     }
 ]
